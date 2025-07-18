2e18b6d0aed52d53515e4badeff05fe7abb7bc62
Harman Singh <118100222+Harman0817@users.noreply.github.com>
2025-07-18 11:02:56 +0530

    Add files via upload

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button"
+const NavItem = () => {
+    return <div className="flex">
+        <div className="relative flex items-center">
+            <Button className="gap-1.5">
+                {/* // here we want to display current category for eg ui-kits , icons etc
+                //  */}
+            </Button>
+        </div>
+    </div>
+}
+
+export default NavItem
